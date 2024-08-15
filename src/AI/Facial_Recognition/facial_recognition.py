62449f5acbd84c0b83c14869c6de126fea4a9aa2
bluepower9 <jarrod.leong@yahoo.com>
2024-08-14 23:24:28 -0700

    added comments to facial recognition.

--- src/AI/Facial_Recognition/facial_recognition.py
+++ src/AI/Facial_Recognition/facial_recognition.py
@@ -11,6 +11,11 @@ ENCODINGS, NAMES = [i[1] for i in __], [i[0] for i in __]
 
 
 def detect_face(frame):
+    '''
+    detects face from a given frame. Converts color scheme from BGR to RGB and 
+    scales image down to 50%. Encodes face and compares it to known faces to
+    determine who the detected face is. Lists as "unknown" if not known.
+    '''
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
     small_frame = cv2.resize(rgb_frame, (0, 0), fx=0.5, fy=0.5)
@@ -22,11 +27,13 @@ def detect_face(frame):
     for enc in face_encs:
         name = 'Unknown'
         matches = fr.compare_faces(ENCODINGS, enc)
+
         if True in matches:
             # face_dist = fr.face_distance(ENCODINGS, enc)
             # smallest_dist = np.argmin(face_dist)
             # name = NAMES[smallest_dist]
-            name = NAMES[matches.index(True)]
+
+            name = NAMES[matches.index(True)]   # gets the first match and returns that name
         
         names.append(name)
 
@@ -45,10 +52,12 @@ def detect_face(frame):
 
 
 def facial_rec_loop():
+    '''
+    Main loop to run facial recognition via webcam.
+    '''
     cv2.namedWindow("preview")
     vc = cv2.VideoCapture(0)
-    waitval = 1
-    missing = 1
+
     if vc.isOpened(): # try to get the first frame
         rval, frame = vc.read()
 
